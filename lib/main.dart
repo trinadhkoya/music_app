dd171a71256807024990ae81669296613050da62
trinadh.koya <mmtrinadhkoya>
2020-06-22 17:21:50 +0530

    fonts refractored to Avenir from Montserrat

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        fontFamily:'Montserrat'
-
-      ),
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: 'Avenir'),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
